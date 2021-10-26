1834ddcf614757418df91babf2d9dd98969f2d42
Endreas Yohannes <endreas@hotmail.ca>
2021-10-26 14:45:33 -0400

    173rd heroku fix attempt

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const {Client, Collection, Intents} = require('discord.js');
 const client = new Client({intents: [Intents.FLAGS.GUILDS]});
 const fs = require('fs');
 const token = process.env.TOKEN;
-const prefix = process.env.PREFIX
+const prefix = process.env.PREFIX;
 
 client.commands = new Collection();
 client.cooldowns = new Collection();
